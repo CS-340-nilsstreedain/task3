818281fbb91f3a3602bb4b5aaa8e2b8e47d8128e
Nils Streedain <nils@streedain.com>
2023-05-12 15:53:12 -0700

    Completed query5.sql

--- query5.sql
+++ query5.sql
@@ -2,4 +2,18 @@
 -- Order the results by film_title in ascending order.
 
 -- Put your query for Q5 here.
-
+SELECT f.title AS film_title
+FROM film f
+WHERE EXISTS (
+	SELECT 1
+	FROM film_actor fa1
+	JOIN actor a1 ON fa1.actor_id = a1.actor_id
+	WHERE f.film_id = fa1.film_id AND a1.first_name = 'RALPH' AND a1.last_name = 'CRUZ'
+)
+AND EXISTS (
+	SELECT 1
+	FROM film_actor fa2
+	JOIN actor a2 ON fa2.actor_id = a2.actor_id
+	WHERE f.film_id = fa2.film_id AND a2.first_name = 'WILL' AND a2.last_name = 'WILSON'
+)
+ORDER BY film_title ASC;
